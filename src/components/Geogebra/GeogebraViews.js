69ce186623cc7ab0cd96ddfb43d84133cfa756ec
Iwo Herka <iwo.herka@makimo.pl>
2019-08-30 12:24:18 +0000

    Fix linter errors; add lint npm command

--- src/components/Geogebra/GeogebraViews.js
+++ src/components/Geogebra/GeogebraViews.js
@@ -66,7 +66,7 @@ export default class {
         api.service('elements').on('created', (element) => {
             this.log.debug('Created element', element.name, element.workshop);
             const pos = this.workshopIds.indexOf(element.workshop);
-          
+
             if (pos !== -1) {
                 this.GAVs[pos].setElement(element);
             }
@@ -75,7 +75,7 @@ export default class {
         api.service('elements').on('patched', (element) => {
             this.log.debug('Patched element', element.name, element.workshop);
             const pos = this.workshopIds.indexOf(element.workshop);
-          
+
             if (pos !== -1) {
                 this.GAVs[pos].updateElementXML(element.name, element.xml);
             }
@@ -84,7 +84,7 @@ export default class {
         api.service('elements').on('removed', (element) => {
             this.log.debug('Removed element', element.name, element.workshop);
             const pos = this.workshopIds.indexOf(element.workshop);
-          
+
             if (pos !== -1) {
                 this.GAVs[pos].deleteObject(element.name);
             }
@@ -93,7 +93,7 @@ export default class {
         api.service('workshops').on('xml-changed', (workshop) => {
             this.log.debug('Created workshop', workshop.name, workshop.id);
             const pos = this.workshopIds.indexOf(workshop.id);
-          
+
             if (pos !== -1) {
                 this.GAVs[pos].setXML(workshop.xml);
             }
