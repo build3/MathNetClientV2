e641e1f4dbf602850d5f8d540bb57738818e64e7
Kamil Supera <kamil.supera@makimo.pl>
2020-07-20 12:12:41 +0200

    Add max width for applet to protect, construction and save correct screen ratio

--- src/components/Pages/Student/StudentGeogebra.vue
+++ src/components/Pages/Student/StudentGeogebra.vue
@@ -139,10 +139,12 @@ export default {
 
         await this.joinWorkshop();
 
+        const maxWidth = (this.$refs.content.clientWidth ? 1600 : this.$refs.content.clientWidth)
+
         const client = new StudentClient({
             container: 'geogebra-view',
-            width: this.$refs.content.clientWidth,
-            height: this.$refs.content.clientWidth * 7 / 18,
+            width: maxWidth,
+            height: maxWidth * 9 / 16,
             log: this.$log,
         });
 
